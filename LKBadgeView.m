4324864651bfe930a80514bb7d5e815c0149b4fa
Jay Zhao <zhaojianyin2012@gmail.com>
2012-09-12 01:59:44 +0800

    Tune stream table display

--- LKBadgeView.m
+++ LKBadgeView.m
@@ -71,7 +71,7 @@
 - (void)_setupDefaultWithoutOutline
 {
     self.textColor = [UIColor whiteColor];
-    self.badgeColor = [UIColor lightGrayColor];
+    self.badgeColor = [UIColor colorWithWhite:0.8 alpha:1];
 
     outline_ = NO;
     outlineWidth_ = 2.0;
